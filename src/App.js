53dbb2f2f80fbb4e1d4dde6f4cf629391d86b023
justinolavides <justinolavides20@gmail.com>
2024-11-23 11:26:56 +0800

    created ui for sign up and log in

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 6c28398 (Created main App component with routing)
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Login from './Login';
 import UserList from './UserList';
 import PrivateRoute from './PrivateRoute';
-<<<<<<< HEAD
 import Register from './Register'; // Import the registration component
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
     return (
@@ -25,49 +22,6 @@ function App() {
             </Routes>
         </Router>
     );
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
->>>>>>> 91111d9 (Initialize project using Create React App)
-=======
-
-
-function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/users" element={
-                    <PrivateRoute>
-                        <UserList />
-                    </PrivateRoute>
-                } />
-                <Route path="*" element={<Navigate to="/login" />} /> {/* Redirect to login by default */}
-            </Routes>
-        </Router>
-    );
->>>>>>> 6c28398 (Created main App component with routing)
 }
-
 
 export default App;
--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,22 +1,10 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
-<<<<<<< HEAD
 const PrivateRoute = ({ children }) => {
     const token = localStorage.getItem('auth_token');
     
     return token ? children : <Navigate to="/" />;
 };
 
-=======
-
-const PrivateRoute = ({ children }) => {
-    const token = localStorage.getItem('auth_token');
-
-
-    return token ? children : <Navigate to="/login" />;
-};
-
-
->>>>>>> fcf4cbb (Created Private Route for User list)
 export default PrivateRoute;
